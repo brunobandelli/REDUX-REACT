3ccd2d69e2ceae4f362801c1ea039fb355940085
brunobandelli <brunobandelli@gmail.com>
2021-10-25 22:37:50 -0300

    Iniciando a APP

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import './Card.css'
+import React from 'react'
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props => {
+    return (
+        <div>
+            Card
+        </div>
+    )
+}
+
