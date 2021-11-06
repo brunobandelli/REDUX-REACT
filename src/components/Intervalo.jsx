e5c5d30c96372bbd335be16701ad7725a65f72a1
brunobandelli <brunobandelli@gmail.com>
2021-11-06 18:58:25 -0300

    CONECTANDO COMPONENTES COM REDUX #02

--- src/components/Intervalo.jsx
+++ src/components/Intervalo.jsx
@@ -1,24 +1,35 @@
 import './Intervalo.css'
 import React from 'react'
+import { connect } from 'react-redux'
 
 import Card from './Card'
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default props => {
-
+function Intervalo(props) {
+    const { min, max } = props
     return (
         <Card title="Intervalo de Números" red>
             <div className="Intervalo">
             <span>
                 <strong>Mìnimo:</strong>
-                <input type="number" value={0} readOnly/>
+                <input type="number" value={min} readOnly/>
             </span>
             <span>
                 <strong>Máximo:</strong>
-                <input type="number" value={10} readOnly/>
+                <input type="number" value={max} readOnly/>
             </span>
             </div>
         </Card>
     )
 
 }
+
+function mapStateToProp(state) {
+    return{
+        min: state.numeros.min,
+        max: state.numeros.max,
+    }
+
+}
+
+export default connect(mapStateToProp)(Intervalo)
--- src/components/Soma.jsx
+++ src/components/Soma.jsx
@@ -1,19 +1,31 @@
 
 import React from 'react'
+import { connect } from 'react-redux'
 
 import Card from './Card'
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default props => {
+function Soma(props){
+    const { min, max } = props
     return (
         <Card title="Soma dos Números" blue>
             <div>
                 <span>
                     <span>Resultado:</span>
-                    <strong>{10}</strong>
+                    <strong>{ max + min }</strong>
                 </span>
             </div>
         </Card>
     )
 
 }
+
+function mapStateToProp(state) {
+    return{
+        min: state.numeros.min,
+        max: state.numeros.max,
+    }
+
+}
+
+export default connect(mapStateToProp)(Soma)
